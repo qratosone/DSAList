e288dc031307c4633d0d7c4f1d9125bd272e2433
qratosone <qratosone@live.com>
2015-08-27 21:43:10 +0800

    Add insertionSort

--- DSAList/ListNode.h
+++ DSAList/ListNode.h
@@ -36,6 +36,12 @@ public:
 		trailer->pred = header;
 		_size = 0;
 	}
+	~List() {
+		Clear();
+		remove(header);
+		remove(trailer);
+		 
+	}
 	Posi(T) find(T const& e, int n, Posi(T) p)const {
 		while (0 < n--) {
 			if (e==(p=p->pred)->data)
@@ -89,9 +95,85 @@ public:
 		_size--;
 		return e;
 	}
-	
-
-};
+	int Clear() {
+		int oldsize = _size;
+		while (0<_size)
+		{
+			remove(header->succ);
+			_size--;
+		}
+		return oldsize;
+	}
+	int deduplicate() {
+		if (_size < 2) return 0;
+		int oldsize = _size;
+		Posi(T) p = first();
+		Rank r = 1;
+		while (trailer!=p->succ)
+		{
+			Posi(T) q = find(p->data, r, p);
+			q ? remove(q) : r++;
+			p = p->succ;
+		}
+		return oldsize - _size;
+	}
+	Posi(T) first() {
+		return header->succ;
+	}
+	int uniquify() {
+		if ((_size < 2)
+		{
+			return 0;
+		}
+		int oldsize = _size;
+		ListNode Posi(T) p = first();
+		ListNode Posi(T) q;
+		while (trailer != (q = p->succ))
+		{
+			if (p->data != q->data) { p = q; }
+			else { remove(q); }
+		}
+		return oldsize - _size;
+	}
+	Posi(T) search(T const &e, int n, Posi(T) p) {
+		while (0<=n)
+		{
+			p = p->pred;
+			if (p->data <= e)break;
+			n--;
+		}
+		return p;
+	}
+	void selectionSort(Posi(T) p, int n) {
+		Posi(T) head = p->pred;
+		Posi(T) tail = p;
+		for (i = 0; i < n; i++) { tail = tail->succ; }
+		while (1<n)
+		{
+			insertBefore(tail, remove(selectMax(head->succ, n));
+			tail = tail->pred;
+			n--;
+		}
+	}
+	Posi(T) selectMax(Posi(T) p,int n){
+		Posi(T) max = p;
+		for (Posi(T) current = p; n>1; n--)
+		{
+			if (current->data>=max->data)
+			{
+				max = current;  //画家算法：max只增加不减少
+			}
+		}
+		return max;
+	}
+	void insertionSort(Posi(T) p, int n) {
+		for (int r = 0; r < n; r++) {
+			insertAfter(search(p->data, r, p), p->data);  //搜索不大于p.data的最大节点，在其后插入p.data
+			p = p->succ;
+			remove(p->pred);         //移动到下一个节点，删除原来的p
+		}
+	}
+};  
 template <typename T>
 T List<T>::operator[](Rank r) const {
 	Posi(T) p = first();
